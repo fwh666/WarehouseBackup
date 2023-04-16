9f82b20f28fca0b24ab8b89cb14b7a01a965a505
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-04-16 17:22:34 +0800

    add：Redis

--- src/main/java/club/fuwenhao/TestController.java
+++ src/main/java/club/fuwenhao/TestController.java
@@ -1,5 +1,7 @@
 package club.fuwenhao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("/test")
     public String test() {
         return "teset";
