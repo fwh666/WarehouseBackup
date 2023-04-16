dfa285e3fd0e447d11d8c946d554a84928861a23
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-04-16 17:17:10 +0800

    add：MySQL

--- src/main/java/club/fuwenhao/ApplicationRun.java
+++ src/main/java/club/fuwenhao/ApplicationRun.java
@@ -1,9 +1,13 @@
 package club.fuwenhao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//todo-fwh-扫描dao包
+//@MapperScan(basePackages = "club.fuwenhao.service")
+//@EnableJpaRepositories(basePackages = "club.fuwenhao.service")
 public class ApplicationRun {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRun.class, args);
